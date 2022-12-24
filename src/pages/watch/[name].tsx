a617f6b074466b5ea9777253c7032c1182ef62e4
Abdul Rahman Al Sbeinaty <abdul@lunapr.io>
2022-12-24 20:12:51 +0400

    updates

--- src/pages/watch/[name].tsx
+++ src/pages/watch/[name].tsx
@@ -6,13 +6,15 @@ import {
   HStack,
   Text,
   Spinner,
+  ModalOverlay,
+  Modal,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
 export async function getServerSideProps(context: any) {
   const path = context.params.name;
   const response = await fetch(
-    `https://anime-scraper-pi.vercel.app//api/getEpNums?path=${path}`
+    `https://anime-scraper-pi.vercel.app/api/getEpNums?path=${path}`
   );
   if (response.status == 404) return { props: { eps: [], path } };
   else {
@@ -30,57 +32,35 @@ type Props = {
 
 const Anime = (props: Props) => {
   const { eps, path } = props;
-  const [link, setLink] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const getLink = async (ep: number) => {
     setIsLoading(true);
     const response = await fetch(
-      `https://anime-scraper-pi.vercel.app//api/getEpLink?path=${path}&ep=${ep}`
+      `https://anime-scraper-pi.vercel.app/api/getEpLink?path=${path}&ep=${ep}`
     );
     const result = await response.json();
+    setIsLoading(false);
+    window.open(`https:${result}`);
     console.log(`https:${result}`);
-    setLink(`https:${result}`);
   };
 
   return (
-    <Box>
-      <Container maxW={"7xl"}>
-        <Box
-          w={"100%"}
-          display={"flex"}
-          flexDir={"column"}
-          alignItems={"center"}
-          my={10}
-          pos={"relative"}
-        >
-          <Box
-            zIndex={1}
-            bgColor={"black"}
-            pos={"absolute"}
-            w={"100%"}
-            h={"100%"}
-            top={0}
-            display={isLoading ? "flex" : "none"}
-            flexDir={"column"}
-            alignItems={"center"}
-            justifyContent={"center"}
-          >
-            <Spinner size={"xl"} />
-          </Box>
-
-          <Box w={"100%"} h={"53.5vw"} maxH={"700px"}>
-            <iframe
-              src={link}
-              allowFullScreen
-              height={"100%"}
-              width={"100%"}
-              frameBorder="0"
-              scrolling="no"
-              onLoad={() => setIsLoading(false)}
-            />
-          </Box>
-        </Box>
-
+    <Box pos={"relative"}>
+      <Box
+        pos={"fixed"}
+        w={"100%"}
+        h={"100%"}
+        top={"0"}
+        display={isLoading ? "flex" : "none"}
+        flexDir={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        zIndex={1}
+        bgColor={"rgb(0,0,0,0.5)"}
+      >
+        <Spinner size={"lg"} />
+      </Box>
+      <Container maxW={"7xl"} my={10}>
         <Grid
           templateColumns={[
             "repeat(3,1fr)",
@@ -92,8 +72,6 @@ const Anime = (props: Props) => {
           ]}
           rowGap={4}
           columnGap={4}
-          maxH={"5xl"}
-          overflowY={"auto"}
         >
           {eps.map((item: number, index: any) => {
             return (
